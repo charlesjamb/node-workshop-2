2822456474c395c8897942c08f2ce67bc649dd48
charlesjamb <charles.jambon@gmail.com>
2016-10-11 14:52:17 -0400

    requestJSON done and npm init done

---
+++ request-as-json.js
@@ -0,0 +1,14 @@
+
+function requestJSON(url, callback) {
+	request(url, function(err, result) {
+		if (err) {
+			callback(err)
+		}
+		try {
+			callback(null, JSON.parse(result.body));
+		}
+		catch(error) {
+			callback(error);
+		}
+	})	
+}
